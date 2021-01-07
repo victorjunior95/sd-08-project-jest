3f2de862313272c8c20ddc6db12c5bf6bd96bc9e
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-01-07 18:53:11 -0300

    Requisito III

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,9 +23,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-
   test('verifica se o usuário é o tunico', async () => {
+    const user = {
+      gender: 'male',
+      name: {first: 'Antônio', last: 'Britto'},
+      location: {country: 'Brazil'},
+      email: 'tunico@bol.com.br',
+      login: {username: 'tunicao123', password: '1234567890'}
+    };
+    api.fetchURL = jest.fn().mockResolvedValue(user);
+
     return api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
