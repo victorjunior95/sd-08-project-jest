0a7b0432eaf1566b6b8d17bdcdd96aee7b8d39c6
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-07 13:52:40 -0300

    Começando projeto jest tests #vqv

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -17,6 +17,6 @@ describe("o retorno do telefonema", () => {
   });
   test("ocupado", () => {
     assert.fail();
-    // Insira seu teste assíncrono aqui
+    // Insira seu teste assíncrono aqui 
   });
 });
