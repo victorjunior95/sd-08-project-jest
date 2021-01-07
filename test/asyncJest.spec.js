365e6742ecaa0c78487b5369493c0edf3a086d5f
vivianeflowt <vivianeflowt@gmail.com>
2021-01-07 16:59:23 -0300

    update

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,16 +13,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
     // assert.fail();
+    expect.assertions(1);
     const result = await answerPhone(true);
     expect(result).toBe('Oi!');
   });
   test('ocupado', async () => {
     // assert.fail();
+    expect.assertions(1);
     try {
       await answerPhone(false);
-    } catch (error) {
-      console.log(error);
-      expect(error).toBe('Infelizmente não podemos atender...');
+    } catch (e) {
+      console.log(e);
+      expect(e).toBe('Infelizmente não podemos atender...');
     }
   });
 });
