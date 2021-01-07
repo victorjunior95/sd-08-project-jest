6c713ec62a63de0ca60d0817c287aba3b404e940
Seu nome <luiza.dilly@gmail.com>
2021-01-07 15:32:14 -0300

    iniciando projeto Jest. REQ1 completo

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
+    expect.assertions(1);
+    const call = await answerPhone(true);
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
     // Insira seu teste assíncrono aqui
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async() => {
+    expect.assertions(1);
+    // const notAnswer = await answerPhone(false);
+    await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
