0a2f6d2d800f665ad4a7545803c8cdf4fc05419f
Mateus Coury <mateuscoury@gmail.com>
2021-01-07 16:21:17 -0300

    mockfunctCC

--- src/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const power = function (base, exponent) {
-  let result = base;
-  for (let count = 0; count < exponent; count++) {
-    result = multiply(result, base)
-  }
-  return result;
-};
-console.log(power(2, 10));
