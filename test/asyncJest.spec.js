bc82c90f67b767e83e86c17927d4931d9528e20c
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-07 16:01:48 -0300

    Requisito 1 - incrementando

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -3,20 +3,20 @@ const answerPhone = require("../src/asyncJest");
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
-
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
-
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    expect.assertions(1);
+    expect.assertions(2);
     answerPhone(true).then(data => expect(data).toEqual('Oi!'));
+    expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test("ocupado", () => {
-    expect.assertions(1);
+    expect.assertions(2);
+    answerPhone(false).catch(data => expect(data).toEqual('Infelizmente não podemos atender...'));
     expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
