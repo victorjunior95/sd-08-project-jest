ed63e859ce020e55621403295c21e8696d091c67
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-07 15:41:42 -0300

    :rocket: develop_wend3ll

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then(answer => {
+      expect(answer).toBe('Oi!');
+    })
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).catch(error => {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    });
   });
-});
+})
