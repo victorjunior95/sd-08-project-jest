1666d8527fcebc31933c2f6adb90dbd3d1a92bfe
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-01-07 15:03:53 -0300

    All 4 reqs, come on Code Climate

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    const answer = await answerPhone(true);
+    expect(answer).toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false);
+    } catch (err) {
+      expect(err).toBe('Infelizmente não podemos atender...');
+    }
   });
 });
