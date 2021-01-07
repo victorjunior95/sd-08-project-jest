e4362325228ee02ea7df1e4e02762d735d4297c4
Daniel <danieldsrd@gmail.com>
2021-01-07 15:24:20 -0300

    Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  
+  const data = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890'
+    }
+  }
+  api.fetchURL = jest.fn().mockResolvedValue(data);
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
