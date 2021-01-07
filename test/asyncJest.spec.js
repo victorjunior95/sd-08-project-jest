4a39d7844032faf61923d7bcf63420a252831e11
Daniel <danieldsrd@gmail.com>
2021-01-07 14:43:18 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,10 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    
+    return answerPhone(true).then(anwser => {
+      expect(anwser).toBe('Oi!');
+    });    
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(error => {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    }); 
   });
 });
