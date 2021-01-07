81b50ebb92f1a62932ce423d43f0dd063eb6ae49
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-01-07 14:53:56 -0300

    R01 - Concluido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    answerPhone('hello').then(result => expect(result).toEqual('Oi!'));
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });.
+    answerPhone().then(result => expect(result).toEqual('Infelizmente não podemos atender...'));
+  });
 });
