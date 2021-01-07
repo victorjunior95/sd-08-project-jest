a9a557e16b3ace80969082c8bd9a4c919af1899b
Patrícia <silva.patricia.v@gmail.com>
2021-01-07 15:35:21 -0300

    Iniciando o Projeto - Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
