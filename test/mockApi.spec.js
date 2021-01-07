2c4c6701d2cc29619d12acea90b50b32d38858fb
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-01-07 16:00:07 -0300

    Requisito 03 v01

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn(() => new Promise(
+    (resolve, reject) => {
+      const user = {
+        gender: 'male',
+        name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+        location: {
+          country: 'Brazil',
+        },
+        email: 'tunico@bol.com.br',
+        login: {
+          username: 'tunicao123',
+          password: '1234567890',
+        },
+      }
+      resolve(user);
+    }));
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
