4be11f26734414740794beb148db1511139d7f28
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-01-07 16:20:57 -0300

    Req3: mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -26,19 +26,19 @@ describe('verifica o usuário', () => {
   api.fetchURL = jest.fn().mockReturnValue(
     new Promise(function (resolve, reject) {
       resolve({
-          gender: 'male',
-          name: {
-            first: 'Antônio',
-            last: 'Britto',
-          },
-          location: {
-            country: 'Brazil'
-          },
-          email: 'tunico@bol.com.br',
-          login: {
-            username: 'tunicao123',
-            password: '1234567890',
-          }
+        gender: 'male',
+        name: {
+          first: 'Antônio',
+          last: 'Britto',
+        },
+        location: {
+          country: 'Brazil'
+        },
+        email: 'tunico@bol.com.br',
+        login: {
+          username: 'tunicao123',
+          password: '1234567890',
+        }
       });
     })
   );
