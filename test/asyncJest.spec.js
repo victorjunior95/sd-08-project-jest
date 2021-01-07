483ad5b7998f6b8212d23b82a26f1693b5658ef6
victor Farias <victorfarias.tv@gmail.com>
2021-01-07 15:00:33 -0300

    Adicionado primeiro requisito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-
+  test("atende", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
     // Insira seu teste assíncrono aqui
   });
-  test("ocupado", () => {
-
+  test("ocupado", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...')
     // Insira seu teste assíncrono aqui
   });
 });
