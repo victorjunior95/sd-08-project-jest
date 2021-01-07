8a12a5c8f27f98f7078a218181a507b07946b45a
Cleberton <clebertonfgc@gmail.com>
2021-01-07 16:29:37 -0300

    Requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
+  
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+   return answerPhone(true).then(resp => {
+    expect(resp).toEqual('Oi!');
+    // expect(answerPhone()).resolves.toBe('Oi!');
+   });
+
   });
+
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(error => {
+      expect(error).toEqual('Infelizmente não podemos atender...');
+      // expect(answerPhone()).rejects.toMatch('Infelizmente não podemos atender...');
+    })
+    
   });
 });
