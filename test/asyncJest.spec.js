1b3af3b3600cefc073fb6c5de779fd2f8785e419
momentts <marcosd3wow@gmail.com>
2021-01-07 14:39:37 -0300

    commit inicial projeto Jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,7 +13,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe("o retorno do telefonema", () => {
   test("atende", () => {
     assert.fail();
-    // Insira seu teste assíncrono aqui
+    function teste({
+      
+    })
   });
   test("ocupado", () => {
     assert.fail();
