fbca0cdfc2d87d2f25816c215983187a50ec576d
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-01-07 15:08:09 -0300

    req 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,9 +1,9 @@
 const mockFunctions = require('../src/mockFunctions');
-
+jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
-Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
+Crie mock functionções de mods para cada uma das operao que os cálculos sejam feitos corretamente,
 não como estão sendo feitos no arquivo original.
 A idéia é que os novos testes sobrescrevam os testes
 importados apenas na suite de testes abaixo.
@@ -16,7 +16,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
-  
+  mockFunctions.add.mockImplementation((a,b) => a + b);
+  mockFunctions.subtract.mockImplementation((a,b) => a - b);
+  mockFunctions.multiply.mockImplementation((a,b) => a * b);
+  mockFunctions.divide.mockImplementation((a,b) => a / b);
+  mockFunctions.power.mockImplementation((a,b) => Math.pow(a,b));
+  mockFunctions.factorial.mockImplementation((a) => {
+    let sum = 1;
+    for (let index = a; index > 0; index-=1){
+      sum = sum * index;
+    }
+    return sum;
+  });
+
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
