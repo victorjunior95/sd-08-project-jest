48d6683cccadcfdb692d223dc3f65e1fb9147dec
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-07 15:53:11 -0300

    Requisito 4 - setupTearDown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,3 +1,4 @@
+const { randomAttack } = require('../src/setupTeardown');
 const adventure = require('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
@@ -20,7 +21,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  jest.mock('../src/setupTeardown')
+  beforeEach( () => {
+    randomAttack();
+  }
+)
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
