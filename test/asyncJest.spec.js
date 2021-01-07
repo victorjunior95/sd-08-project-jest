30784852b5f4ba333e5ffdfbe8afa1184df20b12
Rosiele David <rosieleserpa@hotmail.com>
2021-01-07 15:18:39 -0300

    requisito 1: cria testes para uma funçao assincrona

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    expect.assertions(1);
+    await expect(answerPhone('casa')).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    expect.assertions(1);
+    await expect(answerPhone()).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
