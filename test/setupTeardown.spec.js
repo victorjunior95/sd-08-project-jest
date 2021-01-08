0fcb3568891471010ad0586b32ef8f736d9ce602
liisaambrosen <liisa.ambrosen@gmail.com>
2021-01-08 16:25:10 -0300

    fazendo aparecer o nome no final

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -17,6 +17,7 @@ Sua missão aqui é:
 PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+
 */
 
 describe('quem sobreviveu?', () => {
@@ -29,7 +30,7 @@ describe('quem sobreviveu?', () => {
   })
 
   afterAll(() => {
-    console.log(adventure.specialists);
+    console.log(`O sobrevivente foi ${adventure.specialists[0].nome}`);
   })
 
   test('depois da primeira aventura', () => {
