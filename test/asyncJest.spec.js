94f04d0d3377b4bb18c34a52a0558c2d8f4c716a
v-icto-r <victor130496@gmail.com>
2021-01-08 15:30:02 -0300

    Requsito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(true))
+    .resolves.toBe('Oi!');
+
     // Insira seu teste assíncrono aqui
   });
-  test("ocupado", () => {
-    assert.fail();
+  test("ocupado", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
     // Insira seu teste assíncrono aqui
   });
 });
