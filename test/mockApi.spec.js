382ac8df14a8b47ce054c6be1e50a5f0e804ab0b
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-08 18:40:52 -0300

    Criado um API mock no arquivo test/mockApi.spec.js para que os testes do Jest utilizem retornos de API fixos e independentes de requisições.

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,28 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const userTunico = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  describe('verifica o usuário', () => {
+    const mockApiUrl = jest
+      .spyOn(api, "fetchURL")
+      .mockImplementation(async () => userTunico);
+  });
+  
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
