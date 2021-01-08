dbc3657d3e7e5e92bed702074d89cc6f636f521d
Marcela <marcela9908@gmail.com>
2021-01-08 16:25:07 -0300

    Rquisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone(true).toBe('Oi!'))
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone(false).toBe('Infelizmente não podemos atender...'))
   });
 });
