8484c839f78764682af56c9ad3b33602cb55b8bf
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-08 19:31:17 -0300

    Implementado dentro do describe do arquivo setupTearsown.spec.js, beforeEach para sempre chamar a função randomAttack antes de cada teste

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,21 +20,38 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
+    console.log(`Are alive ${adventure.specialists.map((specialists) => {
+      return specialists.nome;
+    }).toString()}`
+    );
   });
   test('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
+    console.log(`Are alive ${adventure.specialists.map((specialists) => {
+      return specialists.nome;
+    }).toString()}`
+    );
   });
   test('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
+    console.log(`Are alive ${adventure.specialists.map((specialists) => {
+      return specialists.nome;
+    }).toString()}`
+    );
   });
   test('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
+    console.log(`Are alive ${adventure.specialists.map((specialists) => {
+      return specialists.nome;
+    }).toString()}`
+    );
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+    console.log(`The suvivor is ${adventure.specialists[0].nome} the ${adventure.specialists[0].classe}`);
   });
 });
