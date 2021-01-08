230e300215dc232c17d83087d97d569b0ee944dc
Marcela <marcela9908@gmail.com>
2021-01-08 16:08:17 -0300

    Iniciando Projeto

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -3,3 +3,4 @@ const answerPhone = answer => new Promise(function (resolve, reject) {
   setTimeout(answer ? resolve('Oi!') : reject('Infelizmente não podemos atender...'), 3000);
 });
 module.exports = answerPhone;
+//inicio projeto
