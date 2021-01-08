cbe49f317396a1ed4fcac6b0e981b74ceeb762e2
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-01-08 19:31:58 -0300

    Complete requirement 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
@@ -37,4 +39,5 @@ describe('quem sobreviveu?', () => {
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
+
 });
