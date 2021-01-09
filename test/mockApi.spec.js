8d643bb44d4d44f3f9a59c378aed4c8ae1e00fba
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-01-09 16:00:01 -0300

    yezz finish project , very hapy ! , fouth param complete

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,23 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+const objectTunico = {
+  gender: 'male',
+  name: {
+    first: 'Antônio',
+    last: 'Britto'
+  } ,
+  location: {
+    country: 'Brazil'
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890'
+  }
+}
 describe('verifica o usuário', () => {
-  
+  api.fetchURL = jest.fn().mockResolvedValue(objectTunico)
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
