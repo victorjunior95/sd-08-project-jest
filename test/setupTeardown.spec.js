ec5e77302e920b8e64a554aec55fdde4e9e14fb7
jnpefo <jnpefo@gmail.com>
2021-01-08 21:35:25 -0300

    Req 4, tentativa 1

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,21 +20,28 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  const Attack = jest.spyOn(adventure, 'randomAttack');
+
 
   test('depois da primeira aventura', () => {
+    Attack();
     expect(adventure.specialists.length).toBe(5);
   });
+
   test('depois da segunda aventura', () => {
+    Attack();
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
+    Attack();
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
+    Attack();
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
+    Attack();
     expect(adventure.specialists.length).toBe(1);
   });
 });
