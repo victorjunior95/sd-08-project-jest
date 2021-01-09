d7861290ffcedbf6c7e0946d75e606a25bacc438
JRGCast <joao.ralph1@gmail.com>
2021-01-09 08:42:17 -0300

    trocado randomAttack em cada test pelo beforeEach, uma mao na roda
    '

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -7,7 +7,7 @@ Após cada aventura um de nossos aventureiros cairá.
 Cada um dos testes abaixo verifica a quantidade de aventureiros após cada iteração.
 Sua missão aqui é:
 
-  - Use a função randomAttack do objeto adventure
+  - Useobjeto adventure
   que remove um dos aventureiros toda vez que é chamada,
   ela deve funcionar entre cada teste.
   Opcional:
@@ -21,29 +21,26 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
-  test('depois da primeira aventura', () => {
+  beforeEach(() => {
     adventure.randomAttack();
+  });
+  test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
     console.log(adventure.specialists);
   });
   test('depois da segunda aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(4);
     console.log(adventure.specialists);
   });
   test('depois da terceira aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(3);
     console.log(adventure.specialists);
   });
   test('depois da quarta aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(2);
     console.log(adventure.specialists);
   });
   test('depois da quinta aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(1);
     console.log(
       `O sobrevivente é :${JSON.stringify(adventure.specialists[0].nome)}`,
