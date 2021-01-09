c3241c0da012d13d3bfed007fdec22aed36465ae
Rosiele David <rosieleserpa@hotmail.com>
2021-01-09 13:22:24 -0300

    requisito 2: refatoraçao

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,4 @@
-const mockFunctions = require('../src/mockFunctions');
+const { add, subtract, multiply, divide, power, factorial } = require('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
@@ -18,153 +18,63 @@ jest.mock('../src/mockFunctions');
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
-  mockFunctions.add.mockImplementation((a,b) => a + b);
+  add.mockImplementation((a,b) => a + b);
 
-  mockFunctions.subtract.mockImplementation((a,b) => a - b);
+  subtract.mockImplementation((a,b) => a - b);
 
-  mockFunctions.multiply.mockImplementation((a,b) => a * b);
+  multiply.mockImplementation((a,b) => a * b);
 
-  mockFunctions.divide.mockImplementation((a,b) => a / b);
+  divide.mockImplementation((a,b) => a / b);
 
-  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  power.mockImplementation((a, b) => a ** b);
 
-  mockFunctions.factorial.mockImplementation(a => {
-    let fact = 1;
-    for (let index = 1; index <= a; index += 1) {
-      fact *= index;
-    }
-    return fact;
-  });
+  factorial.mockImplementation(a => (a <= 1 && a >= 0) ? 1 : a * factorial(a - 1));
 
   test('testa função add', () => {
-    expect(mockFunctions.add(1, 2)).toEqual(3);
-    expect(mockFunctions.add).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.add).toHaveBeenCalledWith(1, 2);
-
-    expect(mockFunctions.add(8, 37)).toEqual(45);
-    expect(mockFunctions.add).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.add).toHaveBeenCalledWith(8, 37);
-
-    expect(mockFunctions.add(-11, 25)).toEqual(14);
-    expect(mockFunctions.add).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.add).toHaveBeenCalledWith(-11, 25);
-
-    expect(mockFunctions.add(13, -188)).toEqual(-175);
-    expect(mockFunctions.add).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.add).toHaveBeenCalledWith(13, -188);
-
-    expect(mockFunctions.add(7, 26)).toEqual(33);
-    expect(mockFunctions.add).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.add).toHaveBeenCalledWith(7, 26);
+    expect(add(1, 2)).toEqual(3);
+    expect(add(8, 37)).toEqual(45);
+    expect(add(-11, 25)).toEqual(14);
+    expect(add(13, -188)).toEqual(-175);
+    expect(add(7, 26)).toEqual(33);
   });
 
   test('testa função subtract', () => {
-    expect(mockFunctions.subtract(899, 35)).toEqual(864);
-    expect(mockFunctions.subtract).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.subtract).toHaveBeenCalledWith(899, 35);
-
-    expect(mockFunctions.subtract(-17, 333)).toEqual(-350);
-    expect(mockFunctions.subtract).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.subtract).toHaveBeenCalledWith(-17, 333);
-
-    expect(mockFunctions.subtract(45, 97)).toEqual(-52);
-    expect(mockFunctions.subtract).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.subtract).toHaveBeenCalledWith(45, 97);
-
-    expect(mockFunctions.subtract(23, -108)).toEqual(131);
-    expect(mockFunctions.subtract).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.subtract).toHaveBeenCalledWith(23, -108);
-
-    expect(mockFunctions.subtract(-133, -29)).toEqual(-104);
-    expect(mockFunctions.subtract).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.subtract).toHaveBeenCalledWith(-133, -29);
+    expect(subtract(899, 35)).toEqual(864);
+    expect(subtract(-17, 333)).toEqual(-350);
+    expect(subtract(45, 97)).toEqual(-52);
+    expect(subtract(23, -108)).toEqual(131);
+    expect(subtract(-133, -29)).toEqual(-104);
   });
 
   test('testa função multiply', () => {
-    expect(mockFunctions.multiply(1, 2)).toEqual(2);
-    expect(mockFunctions.multiply).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.multiply).toHaveBeenCalledWith(1, 2);
-
-    expect(mockFunctions.multiply(0, 5)).toEqual(0);
-    expect(mockFunctions.multiply).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.multiply).toHaveBeenCalledWith(0, 5);
-
-    expect(mockFunctions.multiply(-4, 9)).toEqual(-36);
-    expect(mockFunctions.multiply).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.multiply).toHaveBeenCalledWith(-4, 9);
-
-    expect(mockFunctions.multiply(-12, -7)).toEqual(84);
-    expect(mockFunctions.multiply).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.multiply).toHaveBeenCalledWith(-12, -7);
-
-    expect(mockFunctions.multiply(19, 23)).toEqual(437);
-    expect(mockFunctions.multiply).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.multiply).toHaveBeenCalledWith(19, 23);
+    expect(multiply(1, 2)).toEqual(2);
+    expect(multiply(0, 5)).toEqual(0);
+    expect(multiply(-4, 9)).toEqual(-36);
+    expect(multiply(-12, -7)).toEqual(84);
+    expect(multiply(19, 23)).toEqual(437);
   });
 
   test('testa função divide', () => {
-    expect(mockFunctions.divide(169, 13)).toEqual(13);
-    expect(mockFunctions.divide).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.divide).toHaveBeenCalledWith(169, 13);
-
-    expect(mockFunctions.divide(-1900, 5)).toEqual(-380);
-    expect(mockFunctions.divide).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.divide).toHaveBeenCalledWith(-1900, 5);
-
-    expect(mockFunctions.divide(42, 7)).toEqual(6);
-    expect(mockFunctions.divide).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.divide).toHaveBeenCalledWith(42, 7);
-
-    expect(mockFunctions.divide(729, 243)).toEqual(3);
-    expect(mockFunctions.divide).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.divide).toHaveBeenCalledWith(729, 243);
-
-    expect(mockFunctions.divide(1331, 11)).toEqual(121);
-    expect(mockFunctions.divide).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.divide).toHaveBeenCalledWith(1331, 11);
+    expect(divide(169, 13)).toEqual(13);
+    expect(divide(-1900, 5)).toEqual(-380);
+    expect(divide(42, 7)).toEqual(6);
+    expect(divide(729, 243)).toEqual(3);
+    expect(divide(1331, 11)).toEqual(121);
   });
 
   test('testa função power', () => {
-    expect(mockFunctions.power(10, 2)).toEqual(100);
-    expect(mockFunctions.power).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.power).toHaveBeenCalledWith(10, 2);
-
-    expect(mockFunctions.power(2, 10)).toEqual(1024);
-    expect(mockFunctions.power).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.power).toHaveBeenCalledWith(2, 10);
-
-    expect(mockFunctions.power(5, 5)).toEqual(3125);
-    expect(mockFunctions.power).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.power).toHaveBeenCalledWith(5, 5);
-
-    expect(mockFunctions.power(1, 10)).toEqual(1);
-    expect(mockFunctions.power).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.power).toHaveBeenCalledWith(1, 10);
-
-    expect(mockFunctions.power(0, 0)).toEqual(1);
-    expect(mockFunctions.power).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.power).toHaveBeenCalledWith(0, 0);
+    expect(power(10, 2)).toEqual(100);
+    expect(power(2, 10)).toEqual(1024);
+    expect(power(5, 5)).toEqual(3125);
+    expect(power(1, 10)).toEqual(1);
+    expect(power(0, 0)).toEqual(1);
   });
 
   test('testa função factorial', () => {
-    expect(mockFunctions.factorial(5)).toEqual(120);
-    expect(mockFunctions.factorial).toHaveBeenCalledTimes(1);
-    expect(mockFunctions.factorial).toHaveBeenCalledWith(5);
-
-    expect(mockFunctions.factorial(10)).toEqual(3628800);
-    expect(mockFunctions.factorial).toHaveBeenCalledTimes(2);
-    expect(mockFunctions.factorial).toHaveBeenCalledWith(10);
-
-    expect(mockFunctions.factorial(3)).toEqual(6);
-    expect(mockFunctions.factorial).toHaveBeenCalledTimes(3);
-    expect(mockFunctions.factorial).toHaveBeenCalledWith(3);
-
-    expect(mockFunctions.factorial(8)).toEqual(40320);
-    expect(mockFunctions.factorial).toHaveBeenCalledTimes(4);
-    expect(mockFunctions.factorial).toHaveBeenCalledWith(8);
-
-    expect(mockFunctions.factorial(2)).toEqual(2);
-    expect(mockFunctions.factorial).toHaveBeenCalledTimes(5);
-    expect(mockFunctions.factorial).toHaveBeenCalledWith(2);
+    expect(factorial(5)).toEqual(120);
+    expect(factorial(10)).toEqual(3628800);
+    expect(factorial(3)).toEqual(6);
+    expect(factorial(8)).toEqual(40320);
+    expect(factorial(2)).toEqual(2);
   });
 });
