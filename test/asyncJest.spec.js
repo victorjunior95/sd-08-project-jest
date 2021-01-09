a4c690bdfcd8e408257a8b9d8beb05a6becbeae0
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-01-09 02:47:59 -0300

    req1: criando testes para uma função assincrona

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
+  test("atende", () => expect(answerPhone(true)).resolves.toEqual('Oi!'));
+  test("ocupado", () => expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...'));
 });
