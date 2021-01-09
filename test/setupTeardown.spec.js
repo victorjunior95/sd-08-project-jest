6d104c5feb79e55820c4b483c142495155302d45
albertassi88 <albertassi88@gmail.com>
2021-01-09 11:44:54 -0300

    Criacao do teste setupTeardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,20 +21,26 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  
+  
   test('depois da primeira aventura', () => {
+    adventure.specialists.splice(5, 1);
     expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
+    adventure.specialists.splice(4, 1);
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
+    adventure.specialists.splice(3, 1);
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
+    adventure.specialists.splice(2, 1);
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
+    adventure.specialists.splice(1, 1);
     expect(adventure.specialists.length).toBe(1);
   });
 });
