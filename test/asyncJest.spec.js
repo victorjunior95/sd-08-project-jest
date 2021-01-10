7eefa781b4587ed45ae8d1b64d02820a016de4a5
Cainan Coutinho <cainan6697@gmail.com>
2021-01-10 19:42:27 -0300

    Primeiro requisito concluido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,9 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    expect(answerPhone(true).resolves.toEqual('Oi!'));
+    expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test("ocupado", () => {
-    expect(answerPhone(false).reject.toEqual('Infelizmente não podemos atender...'))
+    expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
