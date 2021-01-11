dd9b227d96d6ffa8f61af45ac4957a19e606838a
Ronald Jr <rpintojunior@gmail.com>
2021-01-11 12:33:50 -0300

    Insere O verifica o usuário

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,11 +23,28 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const mockApi = jest.spyOn(api, "fetchURL");
+  mockApi.mockResolvedValue(
+    {
+      genre: 'male',
+      name: {
+        first: 'Antônio',
+        last: 'Britto',
+      },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    }
+  )
 
   test('verifica se o usuário é o tunico', async () => {
-    return api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
+    await api.fetchURL().then((user) => {
+      expect(user.genre).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
       expect(user.location.country).toEqual('Brazil');
