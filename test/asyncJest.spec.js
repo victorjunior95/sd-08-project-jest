e768d37880751430ebeb582f2e6098e4b32e1b52
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-01-11 09:28:30 -0400

    init project

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,8 +11,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", () => {    
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
