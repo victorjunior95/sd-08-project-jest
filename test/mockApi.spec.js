d08d4bded39df704d289b166325fa5cad7cdb366
Bruno Wedley <brunowedley27@gmail.com>
2021-01-12 16:55:50 -0300

    :beetle: Correçoes

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 const response = {
-
+  gender: 'male',
+  name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+  location: {
+    country: 'Brazil',
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890',
+  },
 };
 
 
