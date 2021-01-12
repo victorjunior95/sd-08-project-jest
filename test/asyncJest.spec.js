d192aedc4987dc1aa137b8b79c48cf451fd2ae0f
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-12 20:04:40 -0300

    C1 - 1st Req sent

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    answerPhone(true).then((data) => {
+      expect(data).toEqual('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    answerPhone(true).catch((data) => {
+      expect(data).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
