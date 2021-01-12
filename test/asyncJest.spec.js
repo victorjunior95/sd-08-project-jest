ec798e974ba6c476d0df1cb9fade47c8a6a4223e
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-12 19:25:56 -0300

    add: requirement 01 - tests for answerPhone().

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then(resolve => {
+      expect(resolve).toBe('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(error => {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    });
   });
 });
