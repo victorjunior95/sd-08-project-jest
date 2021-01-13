91ba07c6e9b3aeeedc5188fe0f7af88cd01ddaef
wagnerberna <cabelo29007@gmail.com>
2021-01-13 05:42:08 -0300

    req1

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -3,3 +3,5 @@ const answerPhone = answer => new Promise(function (resolve, reject) {
   setTimeout(answer ? resolve('Oi!') : reject('Infelizmente não podemos atender...'), 3000);
 });
 module.exports = answerPhone;
+
+ansewerPhone();
--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,11 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui*
+  test("atende", async () => {
+    
   });
-  test("ocupado", () => {
+  test("ocupado", async () => {
     assert.fail();
     // Insira seu teste assíncrono aqui
   });
