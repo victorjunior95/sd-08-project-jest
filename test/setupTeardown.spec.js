537535021d96ecb5f465e5f69416d829ba0fe641
MuriloBatista <mub4si@gmail.com>
2021-01-13 11:34:23 -0300

    Req:04

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+    if (adventure.specialists.length > 1) {
+      console.log(`Restam ${adventure.specialists.length} sobreviventes.`);
+    } else {
+      console.log(`${adventure.specialists[0].nome} o ${adventure.specialists[0].classe}, sobreviveu a aventura!`);
+    };
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
