585fdacd0989d61dda782d4264498188874299a8
TandyFW <tandyfw@gmail.com>
2021-01-13 08:19:18 -0300

    Requisito 1 concluido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it("atende", () => {
+    return answerPhone(true).then(value => {
+      expect('Oi!').toBe(value);
+    });
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui.
+
+  it("ocupado", async () => {
+    return answerPhone(false).catch(value => {
+      expect('Infelizmente não podemos atender...').toBe(value);
+    });
   });
 });
