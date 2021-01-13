9bc5b34bf319eaaf52563e5e75c8c719692d0a9a
felipebelarmino <epilefgomes1@gmail.com>
2021-01-13 15:24:17 -0300

    Code climate maluco

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  it("atende", async () => {
-    await expect(answerPhone(true)).resolves.toBe('Oi!');
+  test("atende", () => {
+    assert.fail();
+    // Insira seu teste assíncrono aqui
   });
-  it("ocupado", async () => {
-    await expect(answerPhone(false))
-      .rejects
-      .toBe('Infelizmente não podemos atender...');
+  test("ocupado", () => {
+    assert.fail();
+    // Insira seu teste assíncrono aqui
   });
 });
