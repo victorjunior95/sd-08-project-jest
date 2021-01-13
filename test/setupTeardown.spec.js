6f615a7e25c374d2db5b89153ae27fc505813f7a
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-12 22:17:30 -0300

    add: requirement 04 - tests for setupTeardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -23,18 +23,23 @@ describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
   test('depois da primeira aventura', () => {
+    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
+    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
+    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
+    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
+    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(1);
   });
 });
