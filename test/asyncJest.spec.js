cc7007307c9fbf2d56a1c92ef41bf8a03708b67f
felipebelarmino <epilefgomes1@gmail.com>
2021-01-13 15:16:45 -0300

    1 asyncjest test

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it("atende", () => {
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it("ocupado", () => {
+    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });///
