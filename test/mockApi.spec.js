e9a2546d852d7c5ed4163e966b4656efd2edf460
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-01-13 17:19:13 -0300

    adiciona testes do mockAPI

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,9 +24,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const apiURL = jest.spyOn( api, "fetchURL");
+  
+  const infoTonho = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' }
+  };
 
   test('verifica se o usuário é o tunico', async () => {
-    return api.fetchURL().then((user) => {
+    apiURL.mockResolvedValue(infoTonho);
+
+    return apiURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
