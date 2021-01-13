6e91dbe80397ab41e440f6c6a8a2e0b50f6b9e7e
Rafael <rafaeljavaro750@gmail.com>
2021-01-13 16:03:09 -0300

    Req teste assincrono, utilizando async / await caso resposta seja true or catch error caso false

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    const pickUp = await answerPhone(true);
+    expect(pickUp).toEqual('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toEqual('Infelizmente não podemos atender...');
+    }
   });
 });
