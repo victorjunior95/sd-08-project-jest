478c0b36b526a98e02ad5f328d0e47d35f671018
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-01-13 13:51:25 -0300

    O retorno do telefonema

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,10 +12,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    
+    expect.assertions(1);
+    return answerPhone(true).then(answer => {
+      expect(answer).toEqual('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(answer => {
+      expect(answer).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
