17266e945515d43ca16ec5c30c152608776cae74
Antônio Schappo <antonioschappo@gmail.com>
2021-01-14 19:08:29 -0300

    fifth try req. 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,9 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", async () => {
-    await expect(answerPhone(true).resolves.toBe('Oi'))
+    await expect((answerPhone(true)).resolves.toBe('Oi'));
   });
   test("ocupado", async () => {
-    await expect(answerPhone(false).rejects.toBe('Infelizmente não podemos atender...'))
+    await expect((answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...'))
   });
 });
