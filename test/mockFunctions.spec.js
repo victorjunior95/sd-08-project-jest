9f444e9ed76e28a42596371e6569a774744d1c92
Rafael Greco <rafaelmgreco@hotmail.com>
2021-01-14 10:34:01 -0300

    req 2 agr na vdd

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,8 +15,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-  
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation(a => (
+    (a === 1 || a === 0) ? 1 : a * mockFunctions.factorial(a - 1)
+  ));
+
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
