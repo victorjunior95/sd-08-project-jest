e65afcf934f4f04a3b291b3608791b864a956fc5
maym4y <jennim.almeida@gmail.com>
2021-01-14 13:29:38 -0300

    asyncJest concluido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
