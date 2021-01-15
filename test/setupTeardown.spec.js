5dc159d8d59faa9456fbf87b922ab99c58d91900
Antônio Schappo <antonioschappo@gmail.com>
2021-01-15 11:27:02 -0300

    first try req. 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -23,18 +23,28 @@ describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
   test('depois da primeira aventura', () => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
     expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
     expect(adventure.specialists.length).toBe(1);
   });
 });
