a6b473f34c0620ef957bda7cd8faf4e8d6caa6d9
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-01-14 23:07:54 -0300

    ajuste

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
 const answerPhone = require("../src/asyncJest");
-const { answerPhone } = require("../src/asyncJest");
+
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
