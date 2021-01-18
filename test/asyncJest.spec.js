eff5bffea148c2219f86834b50a3be0757c3ecb0
Bruno <brunocmoraes97@gmail.com>
2021-01-18 17:30:20 -0300

    Corridingo reequisito 1 novamente porque o gênio aqui esqueceu do return e deu undefined

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,10 +13,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe("o retorno do telefonema", () => {
   answerPhone = jest.fn();
   test("atende", async () => {
-    expect(answerPhone(true)).resolves.toBe('Oi!');
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
 
   test("ocupado", async () => {
-    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
