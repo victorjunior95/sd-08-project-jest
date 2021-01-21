d92dd330c1e07c80eac8e76fc4024ad8705316d3
Andre Sampaio <andresampaiolm@gmail.com>
2021-01-21 17:09:12 -0300

    mockApi test implemented

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const apiURL = jest.spyOn(api, 'fetchURL');
+  apiURL.mockResolvedValue({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
