5c41d8e279882d93f893a56e257624375e33dc5a
Andre Sampaio <andresampaiolm@gmail.com>
2021-01-21 17:20:35 -0300

    setupTeardown setup done

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  const mockRandomAttack = jest.spyOn(adventure, 'randomAttack');
+
+  beforeEach(() => {
+    mockRandomAttack();
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
