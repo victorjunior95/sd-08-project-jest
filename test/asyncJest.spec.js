1bc55d83d7cd7dd64867c1cc2ab94cc524ca80ea
Uibirá <usbarreto@gmail.com>
2021-01-22 13:44:33 -0300

    Iniciando o projeto. LIFT-OFF!

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,6 +1,6 @@
 const answerPhone = require("../src/asyncJest");
 
-/*
+/* #vqv
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
