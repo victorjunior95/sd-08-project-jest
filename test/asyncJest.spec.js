3194bc50bd190359b0581dd0669b25b3f6ec376f
vicoliveir <victorhugoqxd@gmail.com>
2021-01-27 18:31:11 -0300

    sending projetc jest recovery

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
