58eeee0d2e8d3ec78f93e0e250a0ee77cdb68c64
Uibirá <usbarreto@gmail.com>
2021-01-28 18:20:26 -0300

    Req.1 Revisão

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,19 +10,13 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", async () => {
+describe("o retorno do telefonema", () => {
   test("atende", () => {
-    try {
-      const response = answerPhone(true);
-      expect(response).toBe('Oi!')
-    } catch (error) {   
-    }    
+    const response = answerPhone(true);
+    expect(response).resolves.toBe("Oi!");
   });
   test("ocupado", () => {
-    try {
-      const response = answerPhone(false);
-      expect(response).toBe('Infelizmente não podemos atender...')
-    } catch (error) {   
-    }    
-  });  
+    const response = answerPhone(false);
+    expect(response).rejects.toBe("Infelizmente não podemos atender...");
+  });
 });
