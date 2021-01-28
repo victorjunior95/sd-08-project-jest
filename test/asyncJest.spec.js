c413b00ea07cc76d3f423ff0607c6931f8309a9c
Lucas Duque <lucas3rios2014@gmail.com>
2021-01-28 04:07:23 -0300

    Requisito asyncJest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui!!!
+    // assert.fail();
+    return answerPhone(true).then(answer => {
+      expect(answer).toBe('Oi!');
+    })
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    return answerPhone(false).catch(answer => {
+      expect(answer).toBe('Infelizmente não podemos atender...');
+    })
   });
 });
