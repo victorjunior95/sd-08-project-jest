791de18469eb80ad3f6457d7dfe6b670aa065527
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-01-27 23:25:52 -0300

    projeto jest completo

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,8 +15,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-  
+  mockFunctions.add = jest.fn((a, b) => a + b);
+  mockFunctions.subtract = jest.fn((a, b) => a - b);
+  mockFunctions.multiply = jest.fn((a, b) => a * b);
+  mockFunctions.divide = jest.fn((a, b) => a / b);
+  mockFunctions.power = jest.fn((a, b) => a ** b);
+  mockFunctions.factorial = jest.fn(x => {
+    if (x <= 1) return 1;
+    return x * mockFunctions.factorial(x - 1);
+  });
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
