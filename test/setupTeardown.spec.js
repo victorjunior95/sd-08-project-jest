0e231f6ae4c57d8d71abbeb970bb842c4407cc0f
Nícolas Godek <nicolasgodek@gmail.com>
2021-02-01 19:27:07 -0300

    projeto de Nícolas Godek

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+  afterAll(() => {
+    return console.log(`${adventure.specialists[0].nome} is the only one alive.`);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
