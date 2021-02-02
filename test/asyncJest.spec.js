5cc3509c9df7dccf7be9ea056709c01b37157ca5
Pedro Piquini <piquini.pedro@gmail.com>
2021-02-02 11:35:14 -0300

    Requisito 1 e 2 feitos

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    const returnIsTrue = await answerPhone(true);
+    expect(returnIsTrue).toMatch('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false)
+    } catch(error) {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    };
   });
 });
